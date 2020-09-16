75b28626f6bc993e44ccfb512ba331e7ab9057a2
Kiyoshi Masui <kmasui@mit.edu>
2020-09-16 10:03:52 -0400

    example fitters

--- expansion_fitter/fitters.py
+++ expansion_fitter/fitters.py
@@ -1,8 +1,88 @@
 """Least squares fitters."""
 
 import numpy as np
-from scipy import optimize
+from scipy import optimize, linalg
 
 from expansion_fitter import friedmann_integrators
+
+
+class BaseFitter:
+    """This is an abstract base class. ABSs are classes that are useless on thier own, but provide
+    code that can be used by subclasses.
+
+    """
+
+    def data(self):
+        raise NotImplementedError("Not implemented in base class.")
+
+    def uncertainties(self):
+        raise NotImplementedError("Not implemented in base class.")
+
+    def model(self, parameters):
+        """Returns model predictions for the expectation value of the data, as a function of the
+        parameters.
+
+        Parameters are packed into a 1D array.
+
+        """
+        raise NotImplementedError("Not implemented in base class.")
+
+    def weighted_residuals(self, parameters):
+        return (self.data() - self.model(parameters)) / self.uncertainties()
+
+    def fit(self, parameters0):
+        result = optimize.least_squares(self.weighted_residuals, parameters0)
+        pars_est = result.x
+        jacobian = result.jac
+        covariance_inv = np.dot(jacobian.T, jacobian)
+        covariance = linalg.inv(covariance_inv)
+
+        return pars_est, covariance
+
+    def chi_squared(self, parameters):
+        return np.sum(self.weighted_residuals(parameters)**2)
+
+
+class LineFitter(BaseFitter):
+
+    def __init__(self, x, y, y_err):
+
+        self._x = x
+        self._y = y
+        self._y_err = y_err
+
+    def data(self):
+        return self._y
+
+    def uncertainties(self):
+        return self._y_err
+
+    def model(self, parameters):
+        m = parameters[0]
+        b = parameters[1]
+        return m * self._x + b
+
+
+class LineFitterWithSlopeMeasurement(LineFitter):
+
+    def __init__(self, x, y, y_err, slope_meas, slope_err):
+        LineFitter.__init__(self, x, y, y_err)
+        self._slope_meas = slope_meas
+
+        self._slope_err = slope_err
+
+    def model(self, parameters):
+        y_model = LineFitter.model(self, parameters)
+        return np.concatenate([y_model, [parameters[0]]])
+
+    def data(self):
+        y_data = LineFitter.data(self)
+        return np.concatenate([y_data, [self._slope_meas]])
+
+    def uncertainties(self):
+        y_errs = LineFitter.uncertainties(self)
+        return np.concatenate([y_errs, [self._slope_err]])
+
+
 
 
--- expansion_fitter/tests/test_fitters.py
+++ expansion_fitter/tests/test_fitters.py
@@ -1,4 +1,84 @@
 """Unit tests for least-squares fitters."""
 
+import pytest
+
+import numpy as np
+from numpy import random
+
+from expansion_fitter import fitters
 
 
+def test_constant():
+    """Set data to be constant, make sure we recover 0 slope, constant offset,
+    with uncertainty for offset equal to the formula for the uncertainty in the mean."""
+    n = 64
+    value = 5
+    err = 3
+    x = np.arange(n) - (n - 1) / 2
+    y = value * np.ones(n)
+    y_err = err * np.ones(n)
+    fitter = fitters.LineFitter(x, y, y_err)
+
+    # Initial guess for parameters.
+    pars0 = np.array([1., 1.])
+
+    pars, cov = fitter.fit(pars0)
+    m = pars[0]
+    sig_m = np.sqrt(cov[0, 0])
+    b = pars[1]
+    sig_b = np.sqrt(cov[1, 1])
+    assert b == pytest.approx(value, rel=0.001)
+    assert m == pytest.approx(0, abs=0.0001)
+    assert sig_b == pytest.approx(err / np.sqrt(n), rel=0.01)
+
+
+def test_line():
+    n = 256
+    b_true = 5
+    m_true = 0.5
+    uncertainty = 0.001
+    x = np.arange(n)
+    y = m_true * x + b_true + random.randn(n) * uncertainty
+    y_err = uncertainty * np.ones(n)
+    fitter = fitters.LineFitter(x, y, y_err)
+
+    # Initial guess for parameters.
+    pars0 = np.array([1., 1.])
+
+    pars, cov = fitter.fit(pars0)
+    m = pars[0]
+    sig_m = np.sqrt(cov[0, 0])
+    b = pars[1]
+    sig_b = np.sqrt(cov[1, 1])
+    assert b == pytest.approx(b_true, 0.01)
+    assert m == pytest.approx(m_true, 0.01)
+    # The tolerence for this test based on the variance for a chi-squared distribution.
+    assert fitter.chi_squared(pars) == pytest.approx(n - 2, abs=5 * np.sqrt(2 * n))
+
+
+def test_line_slope_direct_measurement():
+    """By putting all the data at the same x, data does not constrain slope. Information comes from
+    the direct measurement."""
+    n = 64
+    value = 5
+    err = 3
+    x = np.zeros(n)
+    y = value * np.ones(n)
+    y_err = err * np.ones(n)
+    slope_measurement = 3.
+    slope_uncertainty = 1.
+    fitter = fitters.LineFitterWithSlopeMeasurement(x, y, y_err, slope_measurement,
+            slope_uncertainty)
+
+    # Initial guess for parameters.
+    pars0 = np.array([1., 1.])
+
+    pars, cov = fitter.fit(pars0)
+    m = pars[0]
+    sig_m = np.sqrt(cov[0, 0])
+    b = pars[1]
+    sig_b = np.sqrt(cov[1, 1])
+    assert b == pytest.approx(value, rel=0.001)
+    assert m == pytest.approx(slope_measurement, abs=0.01)
+    assert sig_b == pytest.approx(err / np.sqrt(n), rel=0.01)
+    assert sig_m == pytest.approx(slope_uncertainty, rel=0.01)
